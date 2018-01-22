f0e6eb0ecaa6287e72266ce7f41e1c222f4aa7f5
diptargetss <dipsensarma19@gmail.com>
2018-01-22 07:44:55 +0530

    Add files via upload

---
+++ BloodBL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLayer
+{
+    public class BloodBL
+    {
+        BloodBankDA bbda = null;
+        public BloodBL()
+        {
+            bbda = new BloodBankDA();
+        }
+        public bool LoginCheck(admin)
+        {
+            return bbda.Logincheck(admin);
+        }
+        public bool ModifyCampDetails(BloodCamp camp,out string checkmsg)
+        {
+            //bool checkDate=false;
+            if(camp.Date.CompareTo(DateTime.Now)>0)
+            return bbda.ModifyCampDetails();
+            else
+            {
+                //Console.WriteLine("Cannot Change details as date already passed");
+                checkmsg="Cannot Change details as date already passed";
+                return false;
+            }
+        }
+        public bool registerBank(BloodBank b,out string checkmsg)
+        {
+            BloodBank bnk=bbda.BankSearch(b.userid);
+            if(bnk!=null)
+            {
+                checkmsg="Username already exists";
+                return false;
+            }
+            else
+                bbda.registerBank(b);
+
+        }
+        public bool DonorAdd(donor d){ return bbda.DonorAdd(d);}
+
+
+    }
+}
